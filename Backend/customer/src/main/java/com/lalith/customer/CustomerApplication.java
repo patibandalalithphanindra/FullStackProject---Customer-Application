d361f515144e9f1f5d153d77d8d666044c2700cd
patibandalalithphanindra <71619611+patibandalalithphanindra@users.noreply.github.com>
2023-09-04 11:40:16 +0530

    Add files via upload

---
+++ Backend/customer/src/main/java/com/lalith/customer/CustomerApplication.java
@@ -0,0 +1,13 @@
+package com.lalith.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerApplication.class, args);
+	}
+
+}
