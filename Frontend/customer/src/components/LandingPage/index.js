62dc523d1cb94a93d124d5c0d9fefb3c84b404a3
patibandalalithphanindra <patibandalp01@gmail.com>
2023-09-10 16:46:37 +0530

    changes

--- Frontend/customer/src/components/LandingPage/index.js
+++ Frontend/customer/src/components/LandingPage/index.js
@@ -1,56 +1,62 @@
 import React from 'react';
-import { Button, Card, CardContent, Grid, Typography } from '@mui/material';
-import { Link,useNavigate } from 'react-router-dom';
+import { Button, Card, Typography } from '@mui/material';
+import { Link, useNavigate } from 'react-router-dom';
+import styles from './styles.module.css';
 
 function LandingPage() {
-  const cardStyle = {
-    cursor: 'pointer',
-    marginBottom: '20px',
-  };
   const navigate = useNavigate();
 
   const handleLogout = () => {
     localStorage.clear();
-    navigate("/");
-  }
-   return (
-    <div>
-        <Button onClick={handleLogout}>Logout</Button>
-      <Typography variant="h4" align="center" gutterBottom>
-        Choose among the below
-      </Typography>
-      <Grid container justifyContent="center">
-        <Grid item xs={12} sm={4}>
-          <Link to="/customer" style={{ textDecoration: 'none' }}>
-            <Card style={cardStyle}>
-              <CardContent>
-                <Typography variant="h5">Customer</Typography>
-                <Typography variant="body2">Click here to view Customer information.</Typography>
-              </CardContent>
-            </Card>
-          </Link>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <Link to="/order" style={{ textDecoration: 'none' }}>
-            <Card style={cardStyle}>
-              <CardContent>
-                <Typography variant="h5">Order</Typography>
-                <Typography variant="body2">Click here to view Order details.</Typography>
-              </CardContent>
-            </Card>
-          </Link>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <Link to="/rewards" style={{ textDecoration: 'none' }}>
-            <Card style={cardStyle}>
-              <CardContent>
-                <Typography variant="h5">Rewards</Typography>
-                <Typography variant="body2">Click here to check Rewards status.</Typography>
-              </CardContent>
-            </Card>
-          </Link>
-        </Grid>
-      </Grid>
+    navigate('/');
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <Typography variant="h6" style={{ color: '#fff' }}>
+          Customer Management System
+        </Typography>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={handleLogout}
+        >
+          Logout
+        </Button>
+      </div>
+      <div className={styles.cardContainer}>
+        <Link to="/customer" className={styles.cardLink}>
+          <Card className={styles.card}>
+            <div className={styles.cardContent}>
+              <Typography variant="h5">Customer</Typography>
+              <Typography variant="body2">
+                Click here to view Customer information.
+              </Typography>
+            </div>
+          </Card>
+        </Link>
+        <Link to="/order" className={styles.cardLink}>
+          <Card className={styles.card}>
+            <div className={styles.cardContent}>
+              <Typography variant="h5">Order</Typography>
+              <Typography variant="body2">
+                Click here to view Order details.
+              </Typography>
+            </div>
+          </Card>
+        </Link>
+        <Link to="/rewards" className={styles.cardLink}>
+          <Card className={styles.card}>
+            <div className={styles.cardContent}>
+              <Typography variant="h5">Rewards</Typography>
+              <Typography variant="body2">
+                Click here to check Rewards status.
+              </Typography>
+            </div>
+          </Card>
+        </Link>
+      </div>
     </div>
   );
 }
