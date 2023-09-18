4040d96fef7d52faece2bc232bd1591624ca64d1
patibandalalithphanindra <patibandalp01@gmail.com>
2023-09-18 10:15:42 +0530

    pagination

--- Frontend/customer/src/components/Order/index.js
+++ Frontend/customer/src/components/Order/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import {
   Table,
   TableBody,
@@ -16,75 +16,68 @@ import {
   DialogTitle,
   Typography,
   InputAdornment,
-} from '@mui/material';
-import axios from 'axios';
-import styles from './styles.module.css';
-import Navbar from '../common/Navbar';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import EditIcon from '@mui/icons-material/Edit';
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import DeleteIcon from '@mui/icons-material/Delete';
-import AddIcon from '@mui/icons-material/Add';
-import OrderModal from './OrderModal';
-import { Search } from '@mui/icons-material';
-
-const formatDate = (dateString) => {
-  const options = {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-    hour: '2-digit',
-    minute: '2-digit'
-  };
-  return new Date(dateString).toLocaleDateString('en-US', options);
-};
+  TablePagination,
+  TableFooter,
+} from "@mui/material";
+import axios from "axios";
+import styles from "./styles.module.css";
+import Navbar from "../common/Navbar";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import EditIcon from "@mui/icons-material/Edit";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import DeleteIcon from "@mui/icons-material/Delete";
+import AddIcon from "@mui/icons-material/Add";
+import OrderModal from "./OrderModal";
+import { Search } from "@mui/icons-material";
+import TablePaginationActions from "@mui/material/TablePagination/TablePaginationActions";
 
 function Order() {
   const [orders, setOrders] = useState([]);
-  const [searchCustomerId, setSearchCustomerId] = useState('');
+  const [searchCustomerId, setSearchCustomerId] = useState("");
   const [deleteOrderId, setDeleteOrderId] = useState(null);
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
   const [isViewModalOpen, setIsViewModalOpen] = useState(false);
   const [isOrderModalOpen, setIsOrderModalOpen] = useState(false);
   const [selectedOrder, setSelectedOrder] = useState(null);
   const [orderModalData, setOrderModalData] = useState({
-    customerId: '',
+    customerId: "",
     totalItems: 0,
     orderTotal: 0.0,
-    currency: 'INR',
-    customerPhoneNo: '',
-    orderStatus: 'Created',
+    currency: "INR",
+    customerPhoneNo: "",
+    orderStatus: "Created",
   });
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
-  const [withCoinsData, setWithCoinsData] = useState('yes');
-
+  const [withCoinsData, setWithCoinsData] = useState("yes");
 
   useEffect(() => {
-    const response = localStorage.getItem('jwt');
+    const response = localStorage.getItem("jwt");
     const headers = {
       Authorization: `Bearer ${response}`,
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     };
 
     axios
-      .get('http://localhost:8080/orders', { headers })
+      .get("http://localhost:8080/orders", { headers })
       .then((response) => {
         setOrders(response.data);
       })
       .catch((error) => {
-        console.error('Error fetching order data:', error);
+        console.error("Error fetching order data:", error);
       });
   }, []);
 
   const handleAddition = () => {
     setOrderModalData({
-      customerId: '',
+      customerId: "",
       totalItems: 0,
       orderTotal: 0,
-      currency: 'INR',
-      customerPhoneNo: '',
-      orderStatus: 'Created',
+      currency: "INR",
+      customerPhoneNo: "",
+      orderStatus: "Created",
     });
 
     setWithCoinsData(withCoinsData);
@@ -94,6 +87,12 @@ function Order() {
   const dataToSend = {
     orderModalData,
     withCoinsData,
+  };
+
+  const getVisibleOrders = () => {
+    const startIndex = page * rowsPerPage;
+    const endIndex = startIndex + rowsPerPage;
+    return filteredOrders.slice(startIndex, endIndex);
   };
 
   const handleUpdate = (orderNo) => {
@@ -120,33 +119,38 @@ function Order() {
   };
 
   const handleDeleteConfirmation = () => {
-    const response = localStorage.getItem('jwt');
+    const response = localStorage.getItem("jwt");
     const headers = {
       Authorization: `Bearer ${response}`,
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     };
 
     axios
       .delete(`http://localhost:8080/orders/${deleteOrderId}`, { headers })
       .then((response) => {
         if (response.status === 200) {
-          toast.success('Order has been deleted successfully', {
-            position: toast.POSITION.BOTTOM_LEFT,autoClose: 900
+          toast.success("Order has been deleted successfully", {
+            position: toast.POSITION.BOTTOM_LEFT,
+            autoClose: 900,
           });
           setOrders((prevOrders) =>
             prevOrders.filter((order) => order.orderNo !== deleteOrderId)
           );
         } else {
-          toast.error('Order cannot be deleted!', {
-            position: toast.POSITION.BOTTOM_LEFT,autoClose: 900
+          toast.error("Order cannot be deleted!", {
+            position: toast.POSITION.BOTTOM_LEFT,
+            autoClose: 900,
           });
         }
         setIsDeleteModalOpen(false);
       })
       .catch((error) => {
-        console.error(`Error deleting the order ${deleteOrderId}: ${error.message}`);
-        toast.error('Order cannot be deleted!', {
-          position: toast.POSITION.BOTTOM_LEFT,autoClose: 900
+        console.error(
+          `Error deleting the order ${deleteOrderId}: ${error.message}`
+        );
+        toast.error("Order cannot be deleted!", {
+          position: toast.POSITION.BOTTOM_LEFT,
+          autoClose: 900,
         });
         setIsDeleteModalOpen(false);
       });
@@ -165,20 +169,20 @@ function Order() {
   const handleOrderModalClose = () => {
     setIsOrderModalOpen(false);
     setOrderModalData({
-      customerId: '',
+      customerId: "",
       totalItems: 0,
       orderTotal: 0,
-      currency: 'INR',
-      customerPhoneNo: '',
-      orderStatus: 'Created',
+      currency: "INR",
+      customerPhoneNo: "",
+      orderStatus: "Created",
     });
   };
 
   const handleOrderModalSave = () => {
-    const response = localStorage.getItem('jwt');
+    const response = localStorage.getItem("jwt");
     const headers = {
       Authorization: `Bearer ${response}`,
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     };
 
     if (orderModalData.orderNo) {
@@ -190,55 +194,79 @@ function Order() {
         )
         .then((response) => {
           if (response.status === 200) {
-            toast.success('Order has been updated successfully', {
-              position: toast.POSITION.BOTTOM_LEFT,autoClose: 900
+            toast.success("Order has been updated successfully", {
+              position: toast.POSITION.BOTTOM_LEFT,
+              autoClose: 900,
             });
             setOrders((prevOrders) =>
               prevOrders.map((order) =>
-                order.orderNo === orderModalData.orderNo ? orderModalData : order
+                order.orderNo === orderModalData.orderNo
+                  ? orderModalData
+                  : order
               )
             );
           } else {
-            toast.error('An error occurred while updating the order!', {
-              position: toast.POSITION.BOTTOM_LEFT,autoClose: 900
+            toast.error("An error occurred while updating the order!", {
+              position: toast.POSITION.BOTTOM_LEFT,
+              autoClose: 900,
             });
           }
           setIsOrderModalOpen(false);
         })
         .catch((error) => {
-          console.error(`Error updating the order ${orderModalData.orderNo}: ${error.message}`);
-          toast.error('Failed to update the order. Please try again!', {
-            position: toast.POSITION.BOTTOM_LEFT,autoClose: 900
+          console.error(
+            `Error updating the order ${orderModalData.orderNo}: ${error.message}`
+          );
+          toast.error("Failed to update the order. Please try again!", {
+            position: toast.POSITION.BOTTOM_LEFT,
+            autoClose: 900,
           });
           setIsOrderModalOpen(false);
         });
     } else {
       axios
-        .post('http://localhost:8080/orders', dataToSend, { headers })
+        .post("http://localhost:8080/orders", dataToSend, { headers })
         .then((response) => {
-          if (response.status === 201) {
-            toast.success('Order has been added successfully!', {
-              position: toast.POSITION.BOTTOM_LEFT,autoClose: 900
+          if (response.status === 200) {
+            toast.success("Order has been added successfully", {
+              position: toast.POSITION.BOTTOM_LEFT,
+              autoClose: 900,
             });
             setOrders((prevOrders) => [...prevOrders, response.data]);
           } else {
-            toast.error('An error occurred while adding the order', {
-              position: toast.POSITION.BOTTOM_LEFT,autoClose: 900
+            toast.error("An error occurred while adding the order!", {
+              position: toast.POSITION.BOTTOM_LEFT,
+              autoClose: 900,
             });
           }
           setIsOrderModalOpen(false);
         })
         .catch((error) => {
-          console.error('Error adding the order with coin param:', error);
-          toast.error('Failed to add the order with coin param. Please try again.', {
-            position: toast.POSITION.BOTTOM_LEFT,autoClose: 900
+          console.error(`Error adding the order: ${error.message}`);
+          toast.error("Failed to add the order. Please try again!", {
+            position: toast.POSITION.BOTTOM_LEFT,
+            autoClose: 900,
           });
           setIsOrderModalOpen(false);
         });
     }
   };
 
-  const filteredOrders = orders.filter((order) => order.customerId.includes(searchCustomerId));
+  const filteredOrders = orders.filter((order) =>
+    order.customerId.includes(searchCustomerId)
+  );
+
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - filteredOrders.length) : 0;
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
   return (
     <>
@@ -261,11 +289,10 @@ function Order() {
           }}
         />
         <Button
-          style={{ maxWidth: '200px', maxHeight: '40px', marginTop: '8px' }}
+          style={{ maxWidth: "200px", maxHeight: "40px", marginTop: "8px" }}
           variant="contained"
           className={`${styles.button} ${styles.addOrderButton}`}
-          onClick={handleAddition}
-        >
+          onClick={handleAddition}>
           <AddIcon />
         </Button>
       </div>
@@ -291,7 +318,7 @@ function Order() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {filteredOrders.map((order) => (
+            {getVisibleOrders().map((order) => (
               <TableRow key={order.orderNo} className={styles.tableRow}>
                 <TableCell>{order.orderNo}</TableCell>
                 <TableCell>{order.customerId}</TableCell>
@@ -303,8 +330,7 @@ function Order() {
                     variant="contained"
                     color="primary"
                     className={`${styles.button} ${styles.primaryButton}`}
-                    onClick={() => handleView(order.orderNo)}
-                  >
+                    onClick={() => handleView(order.orderNo)}>
                     <VisibilityIcon />
                   </Button>
                   <Button
@@ -312,28 +338,60 @@ function Order() {
                     variant="contained"
                     color="success"
                     className={`${styles.button} ${styles.secondaryButton}`}
-                    onClick={() => handleUpdate(order.orderNo)}
-                  >
+                    onClick={() => handleUpdate(order.orderNo)}>
                     <EditIcon />
                   </Button>
                   <Button
                     variant="contained"
                     color="error"
                     className={`${styles.button} ${styles.tertiaryButton}`}
-                    onClick={() => handleDelete(order.orderNo)}
-                  >
+                    onClick={() => handleDelete(order.orderNo)}>
                     <DeleteIcon />
                   </Button>
                 </TableCell>
               </TableRow>
             ))}
+            {emptyRows > 0 && (
+              <TableRow style={{ height: 53 * emptyRows }}>
+                <TableCell colSpan={5} />
+              </TableRow>
+            )}
           </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TablePagination
+                rowsPerPageOptions={[
+                  5,
+                  10,
+                  25,
+                  50,
+                  { label: "All", value: filteredOrders.length },
+                ]}
+                colSpan={5}
+                count={filteredOrders.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                SelectProps={{
+                  inputProps: {
+                    "aria-label": "rows per page",
+                  },
+                  native: true,
+                }}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+                ActionsComponent={TablePaginationActions}
+              />
+            </TableRow>
+          </TableFooter>
         </Table>
       </TableContainer>
       <Dialog open={isDeleteModalOpen} onClose={handleDeleteCancel}>
         <DialogTitle>Confirmation</DialogTitle>
         <DialogContent>
-          <DialogContentText> Are you sure you want to delete this order? </DialogContentText>
+          <DialogContentText>
+            {" "}
+            Are you sure you want to delete this order?{" "}
+          </DialogContentText>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleDeleteCancel} color="primary">
@@ -364,20 +422,10 @@ function Order() {
                   <Typography variant="body1">
                     <b>Total Order Amount : </b> {selectedOrder.orderTotal}
                   </Typography>
-                  </div>
-                  <div>
+                </div>
+                <div>
                   <Typography variant="body1">
                     <b>Order Status:</b> {selectedOrder.orderStatus}
-                  </Typography>
-                  </div>
-                  <div>
-                  <Typography variant="body1">
-                    <b>Order Date : </b> {formatDate(selectedOrder.orderDate)}
-                  </Typography>
-                  </div>
-                  <div>
-                  <Typography variant="body1">
-                    <b>Last Modified : </b> {formatDate(selectedOrder.lastModifiedTS)}
                   </Typography>
                 </div>
               </DialogContentText>
@@ -395,7 +443,6 @@ function Order() {
         handleClose={handleOrderModalClose}
         orderData={orderModalData}
         withCoins={withCoinsData}
-
         setWithCoins={setWithCoinsData}
         setOrderData={setOrderModalData}
         handleSave={handleOrderModalSave}
--- Frontend/customer/src/components/Reward/index.js
+++ Frontend/customer/src/components/Reward/index.js
@@ -9,6 +9,7 @@ import {
   Paper,
   TextField,
   InputAdornment,
+  TablePagination,
 } from '@mui/material';
 import axios from 'axios';
 import styles from './styles.module.css'; 
@@ -18,6 +19,8 @@ import { Search } from '@mui/icons-material';
 function Reward() {
   const [rewards, setRewards] = useState([]);
   const [searchCustomerId, setSearchCustomerId] = useState('');
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
 
   useEffect(() => {
     const response = localStorage.getItem('jwt');
@@ -44,24 +47,42 @@ function Reward() {
     reward.customerId.includes(searchCustomerId)
   );
 
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - filteredRewards.length) : 0;
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const getVisibleRewards = () => {
+    const startIndex = page * rowsPerPage;
+    const endIndex = startIndex + rowsPerPage;
+    return filteredRewards.slice(startIndex, endIndex);
+  };
+
   return (
     <>
       <Navbar />
-     <div className={styles.headerpart}> 
-     <h3 className={styles.heading}><b>REWARDS INFORMATION</b></h3>
-     <TextField
-    label="Search Customer ID"
-    variant="outlined"
-    value={searchCustomerId}
-    onChange={handleSearch}
-    InputProps={{
-      endAdornment: (
-        <InputAdornment position="end">
-          <Search />
-        </InputAdornment>
-      ),
-    }}
-  />
+      <div className={styles.headerpart}> 
+        <h3 className={styles.heading}><b>REWARDS INFORMATION</b></h3>
+        <TextField
+          label="Search Customer ID"
+          variant="outlined"
+          value={searchCustomerId}
+          onChange={handleSearch}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <Search />
+              </InputAdornment>
+            ),
+          }}
+        />
       </div>
      
       <TableContainer component={Paper} className={styles.container}>
@@ -75,7 +96,7 @@ function Reward() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {filteredRewards.map((reward) => (
+            {getVisibleRewards().map((reward) => (
               <TableRow key={reward.rewardsId} className={styles.tableRow}>
                 <TableCell>{reward.rewardsId}</TableCell>
                 <TableCell>{reward.customerId}</TableCell>
@@ -83,9 +104,24 @@ function Reward() {
                 <TableCell>{reward.rewardsEarned}</TableCell>
               </TableRow>
             ))}
+            {emptyRows > 0 && (
+              <TableRow style={{ height: 53 * emptyRows }}>
+                <TableCell colSpan={4} />
+              </TableRow>
+            )}
           </TableBody>
         </Table>
       </TableContainer>
+      
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25, 50, { label: 'All', value: filteredRewards.length }]}
+        component="div"
+        count={filteredRewards.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
     </>
   );
 }
