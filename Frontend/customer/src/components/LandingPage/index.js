1207f649f81e56658b75ed1817a3c1e41a2fc66e
patibandalalithphanindra <patibandalp01@gmail.com>
2023-10-04 11:20:25 +0530

    status counts

--- Frontend/customer/src/components/LandingPage/index.js
+++ Frontend/customer/src/components/LandingPage/index.js
@@ -1,31 +1,47 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import {  Grid, Paper, Box, Divider } from '@mui/material';
-import { People, ShoppingCart, Star} from '@mui/icons-material';
+import { Grid, Paper, Box, Divider } from "@mui/material";
+import {
+  CheckCircleOutline,
+  FlightTakeoff,
+  LocalShipping,
+  People,
+  ShoppingCart,
+  Star,
+} from "@mui/icons-material";
 
-import { Card, Typography } from '@mui/material';
-import { Link } from 'react-router-dom';
-import styles from './styles.module.css';
-import Navbar from '../common/Navbar';
-
-
+import { Card, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import styles from "./styles.module.css";
+import Navbar from "../common/Navbar";
 
 function LandingPage() {
   const [counts, setCounts] = useState([]);
+  const [statusCounts, setStatusCounts] = useState({});
+
   const fetchCustomerData = () => {
-    const response = localStorage.getItem('jwt');
+    const response = localStorage.getItem("jwt");
     const headers = {
       Authorization: `Bearer ${response}`,
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     };
 
     axios
-      .get('http://localhost:8080/customers/counts', { headers })
+      .get("http://localhost:8080/customers/counts", { headers })
       .then((response) => {
         setCounts(response.data);
       })
       .catch((error) => {
-        console.error('Error fetching customer counts data:', error);
+        console.error("Error fetching customer counts data:", error);
+      });
+
+    axios
+      .get("http://localhost:8080/orders/statuscounts", { headers })
+      .then((response) => {
+        setStatusCounts(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching order status counts data:", error);
       });
   };
 
@@ -35,45 +51,53 @@ function LandingPage() {
 
   return (
     <div className={styles.container}>
-      <Navbar/ >
-      <Paper elevation={3} sx={{ p: 2,width: "300", marginTop:"20px",marginLeft:"200px", marginRight:"200px" }}  >
-      <Grid align="center" container spacing={6} >
-        <Grid item xs={4}>
-          <Box display="flex" alignItems="center">
-          <People sx={{fontSize:60}} />
-            &nbsp; &nbsp;
-            <div>
-              <Typography variant="h6">Customers Onboarded</Typography>
-              <Typography variant="subtitle1">{counts[0]} </Typography>
-            </div>
-          </Box>
+      <Navbar />
+      <Paper
+        elevation={3}
+        sx={{
+          p: 2,
+          width: "300",
+          marginTop: "20px",
+          marginLeft: "200px",
+          marginRight: "200px",
+        }}>
+        <Grid align="center" container spacing={6}>
+          <Grid item xs={4}>
+            <Box display="flex" alignItems="center">
+              <People sx={{ fontSize: 60 }} />
+              &nbsp; &nbsp;
+              <div>
+                <Typography variant="h6">Customers Onboarded</Typography>
+                <Typography variant="subtitle1">{counts[0]} </Typography>
+              </div>
+            </Box>
+          </Grid>
+          <Grid item xs={4}>
+            <Box display="flex" alignItems="center">
+              <ShoppingCart color="primary" sx={{ fontSize: 60 }} />
+              &nbsp;
+              <div>
+                <Typography variant="h6">Total Orders Placed</Typography>
+                <Typography variant="subtitle1">{counts[1]} </Typography>
+              </div>
+            </Box>
+          </Grid>
+          <Grid item xs={4}>
+            <Box display="flex" alignItems="center">
+              <Star color="gold" sx={{ fontSize: 60 }} />
+              &nbsp;
+              <div>
+                <Typography variant="h6">Rewards Issued</Typography>
+                <Typography variant="subtitle1">{counts[2]} </Typography>
+              </div>
+            </Box>
+          </Grid>
         </Grid>
-        <Grid item xs={4}>
-          <Box display="flex" alignItems="center">
-          <ShoppingCart color='primary' sx={{ fontSize: 60 }} />
-            &nbsp;
-            <div>
-              <Typography variant="h6">Total Orders Placed</Typography>
-              <Typography variant="subtitle1">{counts[1]} </Typography>
-            </div>
-          </Box>
-        </Grid>
-        <Grid item xs={4}>
-          <Box display="flex" alignItems="center">
-          <Star color='gold'   sx={{ fontSize: 60 }} />
-            &nbsp; 
-            <div>
-              <Typography variant="h6">No of Rewards Issued</Typography>
-              <Typography variant="subtitle1">{counts[2]} </Typography>
-            </div>
-          </Box>
-        </Grid>
-      </Grid>
-      <Divider sx={{ my: 2 }} />
-      <Typography variant="body2" color="textPrimary">
-        <b>Welcome! Click on respective card to know more detailed info. </b>
-      </Typography>
-    </Paper>
+        <Divider sx={{ my: 2 }} />
+        <Typography variant="body2" color="textPrimary">
+          <b>Welcome! Click on respective card to know more detailed info. </b>
+        </Typography>
+      </Paper>
       <div className={styles.cardContainer}>
         <Link to="/customers/" className={styles.cardLink}>
           <Card className={styles.card}>
@@ -106,10 +130,89 @@ function LandingPage() {
           </Card>
         </Link>
       </div>
-      {/* <div>
-      <img src='/oms.jpg' alt="" height = "280px" width="400px" className={styles.image} />
-      <img src='/oms2.jpg' alt="" height = "280px" width="400px" className={styles.image2} />
-      </div> */}
+      <Paper
+        elevation={3}
+        sx={{
+          p: 2,
+          width: "300",
+          marginTop: "20px",
+          marginLeft: "200px",
+          marginRight: "200px",
+        }}>
+        <Grid align="center" container spacing={6}>
+          <Grid item xs={4}>
+            <Box display="flex" alignItems="center">
+              <Star color="gold" sx={{ fontSize: 60 }} />
+              &nbsp;
+              <div>
+                <Typography variant="h6">Created</Typography>
+                <Typography variant="subtitle1">
+                  {statusCounts?.Created || 0}
+                </Typography>
+              </div>
+            </Box>
+          </Grid>
+
+          <Grid item xs={4}>
+            <Box display="flex" alignItems="center">
+              <ShoppingCart color="primary" sx={{ fontSize: 60 }} />
+              &nbsp;
+              <div>
+                <Typography variant="h6">Items Packed</Typography>
+                <Typography variant="subtitle1">
+                  {statusCounts?.["Items Packed"] || 0}
+                </Typography>
+              </div>
+            </Box>
+          </Grid>
+
+          <Grid item xs={4}>
+            <Box display="flex" alignItems="center">
+              <FlightTakeoff color="secondary" sx={{ fontSize: 60 }} />
+              &nbsp;
+              <div>
+                <Typography variant="h6">Shipped</Typography>
+                <Typography variant="subtitle1">
+                  {statusCounts?.Shipped}
+                </Typography>
+              </div>
+            </Box>
+          </Grid>
+
+          <Grid item xs={4}>
+            <Box display="flex" alignItems="center">
+              <LocalShipping color="action" sx={{ fontSize: 60 }} />
+              &nbsp;
+              <div>
+                <Typography variant="h6">In Transit</Typography>
+                <Typography variant="subtitle1">
+                  {statusCounts?.["In Transit"] || 0}
+                </Typography>
+              </div>
+            </Box>
+          </Grid>
+
+          <Grid item xs={4}>
+            <Box display="flex" alignItems="center">
+              <CheckCircleOutline color="success" sx={{ fontSize: 60 }} />
+              &nbsp;
+              <div>
+                <Typography variant="h6">Delivered</Typography>
+                <Typography variant="subtitle1">
+                  {statusCounts?.Delivered || 0}
+                </Typography>
+              </div>
+            </Box>
+          </Grid>
+        </Grid>
+        <Divider sx={{ my: 2 }} />
+        <Typography variant="body2" color="textPrimary">
+          <b>
+            This represents the Status Information of Orders Placed by the
+            Customers
+          </b>
+        </Typography>
+      </Paper>
     </div>
   );
 }
