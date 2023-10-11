00f75440774b578472068d9454b0aa1e555fc191
patibandalalithphanindra <patibandalp01@gmail.com>
2023-10-11 19:00:23 +0530

    frontend fixes

--- Frontend/customer/src/components/Customer/Dashboard/Profile/index.js
+++ Frontend/customer/src/components/Customer/Dashboard/Profile/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardContent, Avatar, Typography, Grid } from '@mui/material';
 import styles from './styles.module.css';
+
 const Profile = ({ customer }) => {
   const {
     firstName,
@@ -21,43 +22,46 @@ const Profile = ({ customer }) => {
       <CardContent>
         <Grid container spacing={2} alignItems="center">
           <Grid item xs={2} marginLeft={2}>
-            <Avatar src={"https://www.pngitem.com/pimgs/m/62-625606_computer-icons-suit-image-avatar-clip-art-customer.png"}
-             alt="Customer Avatar" sx={{ width: 150, height: 150 }} />
+            <Avatar
+              src="https://www.pngitem.com/pimgs/m/62-625606_computer-icons-suit-image-avatar-clip-art-customer.png"
+              alt="Customer Avatar"
+              sx={{ width: 150, height: 150 }}
+            />
           </Grid>
           <Grid item xs={5} className={styles.head}>
-            <Typography color="textPrimary" className={styles.profile}>
-            <b> Name : </b> {firstName} {lastName}
-            </Typography>
-            <Typography color="textPrimary" className={styles.profile}>
-            <b> Mobile Number : </b> {phoneNo}
-            </Typography>
-            <Typography color="textPrimary" className={styles.profile}>
-            <b> Email Id : </b>{emailId}
-            </Typography>
-            <Typography color="textPrimary" className={styles.profile}>
-            <b> Status : </b>{status}
-            </Typography>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+              <Typography color="textPrimary" className={styles.profile}>
+                <b> Name : </b> {firstName} {lastName}
+              </Typography>
+              <Typography color="textPrimary" className={styles.profile}>
+                <b> Mobile Number : </b> {phoneNo}
+              </Typography>
+              <Typography color="textPrimary" className={styles.profile}>
+                <b> Email Id : </b> {emailId}
+              </Typography>
+              <Typography color="textPrimary" className={styles.profile}>
+                <b> Status : </b> {status}
+              </Typography>
+            </div>
           </Grid>
           <Grid item xs={3}>
-            <Typography color="textPrimary" className={styles.address}>
-              <b>Address : </b> {addressLine1}, {addressLine2}
-            </Typography>
-            <br/>
-            <Typography color="textPrimary" className={styles.address}>
-              <b>City : </b> {city}
-            </Typography>
-            <br/>
-            <Typography color="textPrimary" className={styles.address}>
-             <b> State : </b> {state}
-            </Typography>
-            <br/>
-            <Typography color="textPrimary" className={styles.address}>
-             <b>Country : </b> {country}
-            </Typography>
-            <br/>
-            <Typography color="textPrimary" className={styles.address}>
-             <b> Zip Code : </b>{zipCode}
-            </Typography>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+              <Typography color="textPrimary" className={styles.address} style={{marginTop:'18px'}}>
+                <b>Address : </b> {addressLine1}, {addressLine2}
+              </Typography>
+              <Typography color="textPrimary" className={styles.address}>
+                <b>City : </b> {city}
+              </Typography>
+              <Typography color="textPrimary" className={styles.address}>
+                <b> State : </b> {state}
+              </Typography>
+              <Typography color="textPrimary" className={styles.address}>
+                <b>Country : </b> {country}
+              </Typography>
+              <Typography color="textPrimary" className={styles.address}>
+                <b> Zip Code : </b> {zipCode}
+              </Typography>
+            </div>
           </Grid>
         </Grid>
       </CardContent>
