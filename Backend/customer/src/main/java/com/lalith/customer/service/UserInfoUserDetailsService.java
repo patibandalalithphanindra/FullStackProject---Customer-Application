3427be313d40247654dc8ccebe941b47cb650b1b
patibandalalithphanindra <patibandalp01@gmail.com>
2023-10-19 09:16:19 +0530

    removed unused imports

--- Backend/customer/src/main/java/com/lalith/customer/service/UserInfoUserDetailsService.java
+++ Backend/customer/src/main/java/com/lalith/customer/service/UserInfoUserDetailsService.java
@@ -4,17 +4,13 @@ import com.lalith.customer.config.UserInfoUserDetails;
 import com.lalith.customer.model.UserInfo;
 import com.lalith.customer.repository.UserInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-
-import java.util.Collection;
 import java.util.Optional;
-import java.util.stream.Collectors;
+
 
 @Component
 @Service
